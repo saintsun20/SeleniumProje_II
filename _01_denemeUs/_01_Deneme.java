8b24f184ae2686f0d4f51119dc1b54f727b15ec1
musalpsen <musalpsen@gmail.com>
2024-09-13 20:52:13 +0300

    Denem1

---
+++ _01_denemeUs/_01_Deneme.java
@@ -0,0 +1,10 @@
+package _01_denemeUs;
+
+public class _01_Deneme {
+    public static void main(String[] args) {
+
+        //Hello World try.
+
+        System.out.println("Hello World");
+    }
+}
