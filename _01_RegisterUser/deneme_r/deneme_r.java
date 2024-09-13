da37859f30d114675d1be063af6a8b0b30709ce4
saintsun20 <azizgunes@msn.com>
2024-09-14 00:10:33 +0300

    Delete _01_RegisterUser directory

--- _01_RegisterUser/deneme_r/deneme_r.java
+++
@@ -1,7 +0,0 @@
-package deneme_r;
-
-public class deneme_r {
-    public static void main(String[] args) {
-        System.out.println("deneme 1 2 3");
-    }
-}
--- _01_RegisterUser/hasan/hasan/deneme.java
+++
@@ -1,7 +0,0 @@
-package hasan.hasan;
-
-public class deneme {
-    public static void main(String[] args) {
-        System.out.println("hasan");
-    }
-}
